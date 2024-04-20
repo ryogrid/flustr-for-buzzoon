ac71a28f12c8d12969b9fb19d6036a5c29d7c030
Ryo Kanbayashi <ryo.contact@gmail.com>
2024-04-20 18:04:10 +0900

    refactored: replaced ThreadView to EventView and removed needless ThreadView.

--- lib/view/component/event_view.dart
+++ lib/view/component/event_view.dart
@@ -3,13 +3,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:nostr/nostr.dart';
 import 'package:intl/intl.dart';
-import 'package:nostrp2p/controller/reaction_cache_provider/reaction_cache_notifier.dart';
 import 'package:nostrp2p/external/np2p_api.dart';
 
 import '../../const.dart';
 import '../../controller/current_pubhex_provider/current_pubhex_provider.dart';
 import '../../controller/current_sechex_provider/current_sechex_provider.dart';
-import '../../controller/notification_cache_notifier/notification_cache_notifier.dart';
 import '../../controller/reaction_provider/reaction_provider.dart';
 import '../../controller/servaddr_provider/servaddr_provider.dart';
 import '../../external/np2p_util.dart';
@@ -26,10 +24,6 @@ class EventView extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final author = ref.watch(profileProvider(event.pubkey));
-    final pubHex = ref.watch(currentPubHexProvider);
-    final secHex = ref.watch(currentSecHexProvider);
-    final urls = ref.watch(servAddrSettingNotifierProvider.future);
-    final reaction = ref.watch(reactionProvider(event.id));
 
     return Card(
       child: Padding(
@@ -37,87 +31,12 @@ class EventView extends ConsumerWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            switch (classifyPostKind(this.event.tags)) {
-              POST_KIND.REPLY => TextButton(
-                  child: Text(
-                    this.parentScreen == "notification"
-                        ? "Go to reply thread"
-                        : "Reply to " +
-                            extractAsyncValue<ProfileData?, String>(
-                                ref.watch(profileProvider(
-                                    extractEAndPtags(this.event.tags)["p"]!
-                                        .last[1])),
-                                (prof) => prof!.name,
-                                "unknown") +
-                            "'s post",
-                    style: const TextStyle(color: Colors.blue),
-                  ),
-                  onPressed: () => {
-                    Navigator.of(context).push(
-                      MaterialPageRoute(
-                        builder: (_) => ThreadScreen(event: this.event),
-                      ),
-                    ),
-                  },
-                ),
-              POST_KIND.MENTION => Text(
-                  "@" +
-                      extractAsyncValue<ProfileData?, String>(
-                          ref.watch(profileProvider(
-                              extractEAndPtags(this.event.tags)["p"]!.last[1])),
-                          (prof) => prof!.name,
-                          "unknown"),
-                  style: const TextStyle(color: Colors.blue),
-                ),
-              _ => Container(),
-            },
-            switch (classifyPostKind(this.event.tags)) {
-              POST_KIND.REPLY => const SizedBox(height: 4),
-              POST_KIND.MENTION => const SizedBox(height: 4),
-              _ => Container(),
-            },
+            this.parentScreen != "thread" ? buildPostAnnotation1(context, ref) : Container(),
+            this.parentScreen != "thread" ? buildPostAnnotation2(context, ref): Container(),
             Row(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                switch (author) {
-                  AsyncData(value: final authorProf) => Container(
-                      clipBehavior: Clip.antiAlias,
-                      width: 40,
-                      height: 40,
-                      decoration: const BoxDecoration(
-                        shape: BoxShape.circle,
-                      ),
-                      child: GridTile(
-                        child: GestureDetector(
-                          onTap: () {
-                            Navigator.of(context).push(
-                              MaterialPageRoute(
-                                builder: (_) =>
-                                    ProfileScreen(pubHex: event.pubkey),
-                              ),
-                            );
-                          },
-                          child: Image.network(
-                            authorProf == null
-                                ? NO_PROFILE_USER_PICTURE_URL
-                                : authorProf.picture,
-                          ),
-                        ),
-                      ),
-                    ),
-                  AsyncError(:final error, :final stackTrace) => Container(
-                      clipBehavior: Clip.antiAlias,
-                      width: 40,
-                      height: 40,
-                      decoration: const BoxDecoration(
-                        shape: BoxShape.circle,
-                      ),
-                      child: Image.network(
-                        NO_PROFILE_USER_PICTURE_URL,
-                      ),
-                    ),
-                  _ => const SizedBox(),
-                },
+                buildAuthorPic(context, ref),
                 const SizedBox(width: 8),
                 Flexible(
                   child: Column(
@@ -139,101 +58,8 @@ class EventView extends ConsumerWidget {
                                 event.createdAt * 1000))),
                         alignment: Alignment.centerRight,
                       ),
-                      Row(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        children: [
-                          IconButton(
-                            // reply button
-                            onPressed: () async {
-                              showPostDialog(ref, context, "Send reply post",
-                                  (ref, ctx, sendText) {
-                                final secHex = ref.watch(currentSecHexProvider);
-                                final pubHex = ref.watch(currentPubHexProvider);
-                                final servAddr =
-                                    ref.watch(servAddrSettingNotifierProvider);
-
-                                final _ = switch (servAddr) {
-                                  AsyncData(value: final servAddr) =>
-                                    Np2pAPI.publishPost(
-                                        secHex!,
-                                        pubHex!,
-                                        servAddr.getServAddr!,
-                                        sendText,
-                                        constructSpecialPostTags(
-                                            ref, this.event)),
-                                  _ => null,
-                                };
-                                Navigator.of(ctx).pop();
-                              });
-                            },
-                            icon: Icon(
-                              Icons.reply,
-                              color: Colors.grey,
-                            ),
-                          ),
-                          IconButton(
-                            // like button
-                            onPressed: () async {
-                              var servAddrSettting = await urls;
-                              var url = servAddrSettting.getServAddr!;
-                              var _ = switch (reaction) {
-                                AsyncData(value: final reactionVal) =>
-                                  // if already reacted, don't send reaction
-                                  !reactionVal.pubHexs.contains(pubHex!)
-                                      ? Np2pAPI.publishReaction(
-                                          secHex!,
-                                          pubHex!,
-                                          url,
-                                          event.id,
-                                          event.pubkey,
-                                          "+")
-                                      : null,
-                                AsyncValue() => null,
-                              };
-                            },
-                            icon: Icon(
-                              Icons.favorite_border,
-                              color: switch (reaction) {
-                                AsyncData(value: final reactionVal) =>
-                                  reactionVal.pubHexs.length > 0
-                                      ? Colors.pinkAccent
-                                      : Colors.grey,
-                                AsyncValue() => Colors.grey,
-                              },
-                            ),
-                          ),
-                          Text(switch (reaction) {
-                            AsyncData(value: final reactionVal) =>
-                              reactionVal.pubHexs.length > 0
-                                  ? reactionVal.pubHexs.length.toString() + " "
-                                  : "  ",
-                            AsyncValue() => "  ",
-                          }),
-                        ],
-                      ),
-                      Column(
-                        // reacted user list
-                        children: switch (reaction) {
-                          AsyncData(value: final reactionVal) => reactionVal
-                              .pubHexs
-                              .map((e) => Align(
-                                    child: Text(
-                                        switch (ref.watch(profileProvider(e))) {
-                                          AsyncData(value: final authorProf) =>
-                                            authorProf == null
-                                                ? e.substring(0, 9) + "..."
-                                                : authorProf.name + " ",
-                                          _ => e.substring(0, 9) + "..."
-                                        },
-                                        style: const TextStyle(
-                                            color: Colors.pinkAccent)),
-                                    alignment: Alignment.centerRight,
-                                  ))
-                              .toList(),
-                          _ => [],
-                        },
-                      ),
+                      buildReplyAndLikeButton(context, ref),
+                      buildReactedUserList(context, ref),
                     ],
                   ),
                 ),
@@ -242,6 +68,203 @@ class EventView extends ConsumerWidget {
           ],
         ),
       ),
+    );
+  }
+
+  Widget buildPostAnnotation1(BuildContext context, WidgetRef ref){
+    return switch (classifyPostKind(this.event.tags)) {
+      POST_KIND.REPLY => TextButton(
+        child: Text(
+          this.parentScreen == "notification"
+              ? "Go to reply thread"
+              : "Reply to " +
+              extractAsyncValue<ProfileData?, String>(
+                  ref.watch(profileProvider(
+                      extractEAndPtags(this.event.tags)["p"]!
+                          .last[1])),
+                      (prof) => prof!.name,
+                  "unknown") +
+              "'s post",
+          style: const TextStyle(color: Colors.blue),
+        ),
+        onPressed: () => {
+          Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (_) => ThreadScreen(event: this.event),
+            ),
+          ),
+        },
+      ),
+      POST_KIND.MENTION => Text(
+        "@" +
+            extractAsyncValue<ProfileData?, String>(
+                ref.watch(profileProvider(
+                    extractEAndPtags(this.event.tags)["p"]!.last[1])),
+                    (prof) => prof!.name,
+                "unknown"),
+        style: const TextStyle(color: Colors.blue),
+      ),
+      _ => Container(),
+    };
+  }
+
+  Widget buildAuthorPic(BuildContext context, WidgetRef ref){
+    final author = ref.watch(profileProvider(event.pubkey));
+
+    return switch (author) {
+      AsyncData(value: final authorProf) => Container(
+        clipBehavior: Clip.antiAlias,
+        width: 40,
+        height: 40,
+        decoration: const BoxDecoration(
+          shape: BoxShape.circle,
+        ),
+        child: GridTile(
+          child: GestureDetector(
+            onTap: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (_) =>
+                      ProfileScreen(pubHex: event.pubkey),
+                ),
+              );
+            },
+            child: Image.network(
+              authorProf == null
+                  ? NO_PROFILE_USER_PICTURE_URL
+                  : authorProf.picture,
+            ),
+          ),
+        ),
+      ),
+      AsyncError(:final error, :final stackTrace) => Container(
+        clipBehavior: Clip.antiAlias,
+        width: 40,
+        height: 40,
+        decoration: const BoxDecoration(
+          shape: BoxShape.circle,
+        ),
+        child: Image.network(
+          NO_PROFILE_USER_PICTURE_URL,
+        ),
+      ),
+      _ => const SizedBox(),
+    };
+  }
+
+  Widget buildPostAnnotation2(BuildContext context, WidgetRef ref){
+    return switch (classifyPostKind(this.event.tags)) {
+      POST_KIND.REPLY => const SizedBox(height: 4),
+      POST_KIND.MENTION => const SizedBox(height: 4),
+      _ => Container(),
+    };
+  }
+
+  Widget buildReplyAndLikeButton(BuildContext context, WidgetRef ref){
+    final pubHex = ref.watch(currentPubHexProvider);
+    final secHex = ref.watch(currentSecHexProvider);
+    final urls = ref.watch(servAddrSettingNotifierProvider.future);
+    final reaction = ref.watch(reactionProvider(event.id));
+
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: [
+        IconButton(
+          // reply button
+          onPressed: () async {
+            showPostDialog(ref, context, "Send reply post",
+                    (ref, ctx, sendText) {
+                  final secHex = ref.watch(currentSecHexProvider);
+                  final pubHex = ref.watch(currentPubHexProvider);
+                  final servAddr =
+                  ref.watch(servAddrSettingNotifierProvider);
+
+                  final _ = switch (servAddr) {
+                    AsyncData(value: final servAddr) =>
+                        Np2pAPI.publishPost(
+                            secHex!,
+                            pubHex!,
+                            servAddr.getServAddr!,
+                            sendText,
+                            constructSpecialPostTags(
+                                ref, this.event)),
+                    _ => null,
+                  };
+                  Navigator.of(ctx).pop();
+                });
+          },
+          icon: Icon(
+            Icons.reply,
+            color: Colors.grey,
+          ),
+        ),
+        IconButton(
+          // like button
+          onPressed: () async {
+            var servAddrSettting = await urls;
+            var url = servAddrSettting.getServAddr!;
+            var _ = switch (reaction) {
+              AsyncData(value: final reactionVal) =>
+              // if already reacted, don't send reaction
+              !reactionVal.pubHexs.contains(pubHex!)
+                  ? Np2pAPI.publishReaction(
+                  secHex!,
+                  pubHex!,
+                  url,
+                  event.id,
+                  event.pubkey,
+                  "+")
+                  : null,
+              AsyncValue() => null,
+            };
+          },
+          icon: Icon(
+            Icons.favorite_border,
+            color: switch (reaction) {
+              AsyncData(value: final reactionVal) =>
+              reactionVal.pubHexs.length > 0
+                  ? Colors.pinkAccent
+                  : Colors.grey,
+              AsyncValue() => Colors.grey,
+            },
+          ),
+        ),
+        Text(switch (reaction) {
+          AsyncData(value: final reactionVal) =>
+          reactionVal.pubHexs.length > 0
+              ? reactionVal.pubHexs.length.toString() + " "
+              : "  ",
+          AsyncValue() => "  ",
+        }),
+      ],
+    );
+  }
+
+  Widget buildReactedUserList(BuildContext context, WidgetRef ref){
+    final reaction = ref.watch(reactionProvider(event.id));
+
+    return Column(
+      // reacted user list
+      children: switch (reaction) {
+        AsyncData(value: final reactionVal) => reactionVal
+            .pubHexs
+            .map((e) => Align(
+          child: Text(
+              switch (ref.watch(profileProvider(e))) {
+                AsyncData(value: final authorProf) =>
+                authorProf == null
+                    ? e.substring(0, 9) + "..."
+                    : authorProf.name + " ",
+                _ => e.substring(0, 9) + "..."
+              },
+              style: const TextStyle(
+                  color: Colors.pinkAccent)),
+          alignment: Alignment.centerRight,
+        ))
+            .toList(),
+        _ => [],
+      },
     );
   }
 }
--- lib/view/screen/thread_screen.dart
+++ lib/view/screen/thread_screen.dart
@@ -1,20 +1,12 @@
-import 'package:collection/collection.dart';
 import 'package:nostr/nostr.dart';
 import 'package:nostrp2p/controller/event_data_getting_timer_provider/event_data_getting_timer_provider.dart';
-import 'package:nostrp2p/controller/is_seckey_available_provider/is_seckey_available_provider.dart';
-import 'package:nostrp2p/controller/timeline_posts_notifier/timeline_posts_notifier.dart';
+
 import 'package:nostrp2p/view/component/event_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import '../../controller/current_pubhex_provider/current_pubhex_provider.dart';
-import '../../controller/follow_list_provider/follow_list_provider.dart';
-import '../../controller/is_following_only_tl_provider/is_following_only_tl_provider.dart';
 import '../../controller/notification_cache_notifier/notification_cache_notifier.dart';
 import '../../external/np2p_util.dart';
-import '../component/posting_button.dart';
-import '../component/thread_view.dart';
-import '../component/top_bar.dart';
 
 class ThreadScreen extends ConsumerWidget {
   const ThreadScreen({Key? key, required this.event}) : super(key: key);
@@ -30,13 +22,13 @@ class ThreadScreen extends ConsumerWidget {
       body: RefreshIndicator(
         onRefresh: () async => ref.invalidate(eventDataGettingTimerProvider),
         child: ListView(
-          children: constructTheadViewList(ref, this.event),
+          children: constructEventViewList(ref, this.event),
         ),
       ),
     );
   }
 
-  List<Widget> constructTheadViewList(WidgetRef ref, Event event) {
+  List<Widget> constructEventViewList(WidgetRef ref, Event event) {
     var retList = <Widget>[];
 
     final notifications = ref.watch(notificationCacheNotifierProvider);
@@ -44,7 +36,8 @@ class ThreadScreen extends ConsumerWidget {
     // traverse the parent post events
     var parentEvent = notifications.eventDataMap[extractEAndPtags(event.tags)["e"]!.last[1]];
     while (parentEvent != null) {
-      retList.add(ThreadView(event: parentEvent));
+      //retList.add(ThreadView(event: parentEvent));
+      retList.add(EventView(event: parentEvent, parentScreen: "thread"));
       if (extractEAndPtags(parentEvent.tags)["e"]!.length > 0){
         parentEvent = notifications.eventDataMap[extractEAndPtags(parentEvent.tags)["e"]!.last[1]];
       }else{
@@ -53,7 +46,8 @@ class ThreadScreen extends ConsumerWidget {
     }
 
     retList = retList.reversed.toList();
-    retList.add(ThreadView(event: event));
+    //retList.add(ThreadView(event: event));
+    retList.add(EventView(event: event, parentScreen: "thread"));
     return retList;
   }
 }
